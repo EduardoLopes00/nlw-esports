69aab55d830ff5f4c66177b26343ab4247b796eb
Eduardo Lopes <eduardo.toworkstudy@gmail.com>
2022-09-15 03:05:25 -0300

    feat: Add get games endPoint

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,27 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
 
 import cors from "cors";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+const prisma = new PrismaClient();
+
+app.get("/games", async (request, response) => {
+  const games = await prisma.game.findMany({
+    include: {
+      //Inner join do Prisma
+      _count: {
+        select: {
+          ads: true,
+        },
+      },
+    },
+  });
+
+  return response.json(games);
+});
 
 app.listen(3333);
